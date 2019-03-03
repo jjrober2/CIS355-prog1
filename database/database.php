b5ab53385068d673131b5a01fd1159ef4461c2bb
jjrober2 <38956608+jjrober2@users.noreply.github.com>
2019-03-03 12:03:54 -0500

    Add files via upload

---
+++ database/database.php
@@ -0,0 +1,44 @@
+<?php
+class Database {
+  // these are not the real passwords
+  // for real passwords, see file in ../database subdirectory
+  private static $dbName =  'test';
+
+  private static $dbHost =  '127.0.0.1';
+
+  private static $dbUsername =  'root';
+
+  private static $dbUserPassword =  'Rim.sky1844';
+
+  private static $cont =  null;
+
+  public function __construct()
+  {
+		exit('Init function is not allowed');
+  }
+
+  public static function connect()
+  {
+	   // One connection through whole application
+       if ( null == self::$cont )
+       {      
+        try 
+        {
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
+        }
+        catch(PDOException $e) 
+        {
+          die($e->getMessage());  
+        }
+       } 
+       return self::$cont;
+  }
+
+  public static function disconnect()
+  {
+		self::$cont = null;
+  }
+
+}
+
+?>
